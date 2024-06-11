8305d64706eec3f8dfd799b133ad74c1dee08379
maxitrigo <maxitrigo@gmail.com>
2024-06-11 14:18:55 -0300

    Ejercicios C5

--- C5 Fundamentos Javascript/ejercicios/07.js
+++ C5 Fundamentos Javascript/ejercicios/07.js
@@ -3,6 +3,19 @@ function esTipoDato(valor) {
   // Retorna el tipo de dato de este valor.
   // Por ejemplo: "string", "number", "boolean", "object", etc.
   // Tu código:
+  if (typeof valor === 'string') {
+    return 'string';
+  } else if (typeof valor === 'number') {
+    return 'number';
+  } else if (typeof valor === 'boolean') {
+    return 'boolean';
+  } else if (typeof valor === 'object') {
+    return 'object';
+  } else if (typeof valor === 'function') {
+    return 'function';
+  } else {
+    return 'undefined';
+  }
 }
 
 module.exports = esTipoDato;
