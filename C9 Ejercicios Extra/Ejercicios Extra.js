910900bc22648975ecacf995e3f27504169ce452
maxitrigo <maxitrigo@gmail.com>
2024-06-20 16:06:43 -0300

    Resto de los Ejercicios

--- C9 Ejercicios Extra/Ejercicios Extra.js
+++ C9 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,7 @@ function deObjetoAarray(objeto) {
   // Estos elementos debe ser cada par clave:valor del objeto recibido.
   // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
   // Tu código:
+  return Object.entries(objeto);
 }
 
 function numberOfCharacters(string) {
@@ -14,6 +15,31 @@ function numberOfCharacters(string) {
   // Las letras deben estar en orden alfabético.
   // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
   // Tu código:
+  // Crear un objeto vacío para almacenar los conteos
+  const charCount = {};
+
+  // Recorrer cada carácter de la cadena
+  for (let char of string) {
+    // Si el carácter ya existe en el objeto, incrementar su conteo
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      // Si el carácter no existe, inicializar su conteo en 1
+      charCount[char] = 1;
+    }
+  }
+
+  // Crear un arreglo con las claves del objeto y ordenarlas alfabéticamente
+  const sortedKeys = Object.keys(charCount).sort();
+
+  // Crear un nuevo objeto con las claves ordenadas
+  const sortedCharCount = {};
+  for (let key of sortedKeys) {
+    sortedCharCount[key] = charCount[key];
+  }
+
+  // Retornar el objeto con los caracteres y sus conteos en orden alfabético
+  return sortedCharCount;
 }
 
 function capToFront(string) {
@@ -22,6 +48,24 @@ function capToFront(string) {
   // Retornar el string.
   // [EJEMPLO]: soyHENRY ---> HENRYsoy
   // Tu código:
+  
+  // Arreglos para almacenar letras mayúsculas y minúsculas
+  let upperCaseLetters = [];
+  let lowerCaseLetters = [];
+
+  // Recorrer cada letra de la cadena
+  for (let char of string) {
+    // Verificar si la letra es mayúscula
+    if (char === char.toUpperCase()) {
+      upperCaseLetters.push(char);
+    } else {
+      // Si no es mayúscula, agregarla al arreglo de minúsculas
+      lowerCaseLetters.push(char);
+    }
+  }
+
+  // Combinar las letras mayúsculas primero y luego las minúsculas
+  return upperCaseLetters.join('') + lowerCaseLetters.join('');
 }
 
 function asAmirror(frase) {
